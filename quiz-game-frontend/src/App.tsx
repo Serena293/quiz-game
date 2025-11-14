aa93f3f1caa45232e16463653c58204d229dd867
SERENA FERRARO <serenaferraro21@gmail.com>
2025-11-14 14:19:12 +0000

    initial commit

---
+++ quiz-game-frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNavbar from './components/MyNavbar';
+import Footer from './components/Footer';
+
+function App() {
+
+  return (
+    <>
+    <MyNavbar />
+    <h1 className='pt-50'>Quiz Game</h1>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ quiz-game-frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="position-fixed bottom-0 text-center w-100 bg-body-tertiary">
+        <h3>Footer</h3>
+    </div>)
+    }
+
+
+export default Footer
